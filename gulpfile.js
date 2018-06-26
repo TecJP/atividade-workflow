bd724bccfc38b8f6a441e13c6d85a4c389bbd9f0
TecJP <tec.joao.freitas@gmail.com>
2018-06-25 22:42:11 -0300

    Código de minificação do HTML inserido..

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
-let gulp = require('gulp')
+let gulp = require('gulp');
+let htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify_html', function() {
+    return gulp.src('./source/index.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'))
+});
